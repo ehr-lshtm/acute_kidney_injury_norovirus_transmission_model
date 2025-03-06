b7de44358eae4fdac169edfda349678eca1d1013
bolthikaru <67472580+bolthikaru@users.noreply.github.com>
2025-03-06 08:38:34 +0000

    Update 04_likelihood_prior_functions_quasipoisson.R
    remove GP 5-14 from likelihood calculation

--- R/04_likelihood_prior_functions_quasipoisson.R
+++ R/04_likelihood_prior_functions_quasipoisson.R
@@ -27,67 +27,68 @@ log_likelihood_sum_quasi_pois <- function(parameters, init.state, times, data, l
   log_likelihoods <- traj[data, on = "time"][
     , c(
       "log_likelihood_noro_obs_1",
-      "log_likelihood_noro_obs_2",
+      # "log_likelihood_noro_obs_2",
       "log_likelihood_noro_obs_3",
       "log_likelihood_noro_obs_4",
       "log_likelihood_aki_hosp_4",
       "log_likelihood_gastro_hosp_4",
-      "log_likelihood_gastro_gp_1",
-      "log_likelihood_gastro_gp_2"
+      "log_likelihood_gastro_gp_1"
+      # "log_likelihood_gastro_gp_2"
     ) :=
       {
         log_likelihood_noro_obs_1 <- quasi_pois_loglik(y = noro_obs_1, mu = noro_model_1, phi = dispersion_params$noro_obs_1)
-        log_likelihood_noro_obs_2 <- quasi_pois_loglik(y = noro_obs_2, mu = noro_model_2, phi = dispersion_params$noro_obs_2)
+        # log_likelihood_noro_obs_2 <- quasi_pois_loglik(y = noro_obs_2, mu = noro_model_2, phi = dispersion_params$noro_obs_2)
         log_likelihood_noro_obs_3 <- quasi_pois_loglik(y = noro_obs_3, mu = noro_model_3, phi = dispersion_params$noro_obs_3)
         log_likelihood_noro_obs_4 <- quasi_pois_loglik(y = noro_obs_4, mu = noro_model_4, phi = dispersion_params$noro_obs_4)
         log_likelihood_aki_hosp_4 <- quasi_pois_loglik(y = aki_hosp_obs_4, mu = aki_hosp_model_4, phi = dispersion_params$aki_hosp_4)
         log_likelihood_gastro_hosp_4 <- quasi_pois_loglik(y = gastro_hosp_obs_4, mu = gastro_hosp_model_4, phi = dispersion_params$gastro_hosp_obs_4)
         log_likelihood_gastro_gp_1 <- quasi_pois_loglik(y = gastro_gp_obs_1, mu = gastro_gp_model_1, phi = dispersion_params$gastro_gp_1)
-        log_likelihood_gastro_gp_2 <- quasi_pois_loglik(y = gastro_gp_obs_2, mu = gastro_gp_model_2, phi = dispersion_params$gastro_gp_2)
+        # log_likelihood_gastro_gp_2 <- quasi_pois_loglik(y = gastro_gp_obs_2, mu = gastro_gp_model_2, phi = dispersion_params$gastro_gp_2)
         .(
           log_likelihood_noro_obs_1,
-          log_likelihood_noro_obs_2,
+          # log_likelihood_noro_obs_2,
           log_likelihood_noro_obs_3,
           log_likelihood_noro_obs_4,
           log_likelihood_aki_hosp_4,
           log_likelihood_gastro_hosp_4,
-          log_likelihood_gastro_gp_1,
-          log_likelihood_gastro_gp_2
+          log_likelihood_gastro_gp_1
+          # log_likelihood_gastro_gp_2
         )
       }
   ]
   
   # Calculate the sum of log likelihoods
   log_likelihood_noro_obs_1 <- sum(log_likelihoods$log_likelihood_noro_obs_1, na.rm = TRUE)
-  log_likelihood_noro_obs_2 <- sum(log_likelihoods$log_likelihood_noro_obs_2, na.rm = TRUE)
+  # log_likelihood_noro_obs_2 <- sum(log_likelihoods$log_likelihood_noro_obs_2, na.rm = TRUE)
   log_likelihood_noro_obs_3 <- sum(log_likelihoods$log_likelihood_noro_obs_3, na.rm = TRUE)
   log_likelihood_noro_obs_4 <- sum(log_likelihoods$log_likelihood_noro_obs_4, na.rm = TRUE)
   log_likelihood_aki_hosp_4 <- sum(log_likelihoods$log_likelihood_aki_hosp_4, na.rm = TRUE)
   log_likelihood_gastro_hosp_4 <- sum(log_likelihoods$log_likelihood_gastro_hosp_4, na.rm = TRUE)
   log_likelihood_gastro_gp_1 <- sum(log_likelihoods$log_likelihood_gastro_gp_1, na.rm = TRUE)
-  log_likelihood_gastro_gp_2 <- sum(log_likelihoods$log_likelihood_gastro_gp_2, na.rm = TRUE)
+  # log_likelihood_gastro_gp_2 <- sum(log_likelihoods$log_likelihood_gastro_gp_2, na.rm = TRUE)
   
   # Calculate the sum of the sums
   total_log_likelihood <- sum(
     log_likelihood_noro_obs_1,
-    log_likelihood_noro_obs_2,
+    # log_likelihood_noro_obs_2,
     log_likelihood_noro_obs_3,
     log_likelihood_noro_obs_4,
     log_likelihood_aki_hosp_4, 
     log_likelihood_gastro_hosp_4,
-    log_likelihood_gastro_gp_1,
-    log_likelihood_gastro_gp_2
+    log_likelihood_gastro_gp_1
+    # log_likelihood_gastro_gp_2
   )
   
   likelihoods <- list(total_log_likelihood = total_log_likelihood,
                       log_likelihood_noro_obs_1 = log_likelihood_noro_obs_1,
-                      log_likelihood_noro_obs_2 = log_likelihood_noro_obs_2,
+                      # log_likelihood_noro_obs_2 = log_likelihood_noro_obs_2,
                       log_likelihood_noro_obs_3 = log_likelihood_noro_obs_3,
                       log_likelihood_noro_obs_4 = log_likelihood_noro_obs_4,
                       log_likelihood_aki_hosp_4 = log_likelihood_aki_hosp_4,
                       log_likelihood_gastro_hosp_4 = log_likelihood_gastro_hosp_4, 
-                      log_likelihood_gastro_gp_1 = log_likelihood_gastro_gp_1,
-                      log_likelihood_gastro_gp_2 = log_likelihood_gastro_gp_2)
+                      log_likelihood_gastro_gp_1 = log_likelihood_gastro_gp_1
+                      # log_likelihood_gastro_gp_2 = log_likelihood_gastro_gp_2
+                      )
   
   return(ifelse(log, total_log_likelihood, exp(total_log_likelihood)))
   
