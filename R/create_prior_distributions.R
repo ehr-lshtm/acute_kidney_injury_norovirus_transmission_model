9032ef5136bf605d86e26adaa03d2211c74f74b6
bolthikaru <67472580+bolthikaru@users.noreply.github.com>
2025-03-06 08:40:29 +0000

    Update create_prior_distributions.R
    update prior distribution visualisation with seasonal reporting parameter

--- R/create_prior_distributions.R
+++ R/create_prior_distributions.R
@@ -2,22 +2,28 @@ create_prior_df <- function(n_points = 1000) {
   # Create sequences for each parameter
   priors <- list(
     # Uniform priors for surveillance reports
-    surveillance_report_1_summer = list(x = seq(0, 0.01, length.out = n_points), 
-                                 density = dunif(seq(0, 0.01, length.out = n_points), 0, 0.01)),
-    surveillance_report_1_winter = list(x = seq(0, 0.01, length.out = n_points), 
-                                 density = dunif(seq(0, 0.01, length.out = n_points), 0, 0.02)),
-    surveillance_report_2_summer = list(x = seq(0, 0.01, length.out = n_points),
-                                 density = dunif(seq(0, 0.01, length.out = n_points), 0, 0.01)),
-    surveillance_report_2_winter = list(x = seq(0, 0.01, length.out = n_points),
-                                 density = dunif(seq(0, 0.01, length.out = n_points), 0, 0.02)),
-    surveillance_report_3_summer = list(x = seq(0, 0.01, length.out = n_points),
-                                 density = dunif(seq(0, 0.01, length.out = n_points), 0, 0.01)),
-    surveillance_report_3_winter = list(x = seq(0, 0.01, length.out = n_points),
-                                 density = dunif(seq(0, 0.01, length.out = n_points), 0, 0.02)),
-    surveillance_report_4_summer = list(x = seq(0, 0.02, length.out = n_points),
-                                 density = dunif(seq(0, 0.02, length.out = n_points), 0, 0.02)),
-    surveillance_report_4_winter = list(x = seq(0, 0.02, length.out = n_points),
-                                 density = dunif(seq(0, 0.02, length.out = n_points), 0, 0.06)),
+    surveillance_report_1 = list(x = seq(0, 0.06, length.out = n_points), 
+                                 density = dunif(seq(0, 0.06, length.out = n_points), 0, 0.06)),
+    # surveillance_report_1_summer = list(x = seq(0, 0.06, length.out = n_points), 
+    #                              density = dunif(seq(0, 0.06, length.out = n_points), 0, 0.06)),
+    # surveillance_report_1_winter = list(x = seq(0, 0.06, length.out = n_points), 
+    #                              density = dunif(seq(0, 0.06, length.out = n_points), 0, 0.06)),
+    # surveillance_report_2 = list(x = seq(0, 0.06, length.out = n_points),
+    #                              density = dunif(seq(0, 0.06, length.out = n_points), 0, 0.06)),
+    # surveillance_report_2_summer = list(x = seq(0, 0.06, length.out = n_points),
+    #                              density = dunif(seq(0, 0.06, length.out = n_points), 0, 0.06)),
+    # surveillance_report_2_winter = list(x = seq(0, 0.06, length.out = n_points),
+    #                              density = dunif(seq(0, 0.06, length.out = n_points), 0, 0.06)),
+    surveillance_report_3 = list(x = seq(0, 0.06, length.out = n_points),
+                                 density = dunif(seq(0, 0.06, length.out = n_points), 0, 0.06)),
+    # surveillance_report_3_summer = list(x = seq(0, 0.06, length.out = n_points),
+    #                              density = dunif(seq(0, 0.06, length.out = n_points), 0, 0.06)),
+    # surveillance_report_3_winter = list(x = seq(0, 0.06, length.out = n_points),
+    #                              density = dunif(seq(0, 0.06, length.out = n_points), 0, 0.06)),
+    surveillance_report_4_summer = list(x = seq(0, 0.06, length.out = n_points),
+                                 density = dunif(seq(0, 0.1, length.out = n_points), 0, 0.1)),
+    surveillance_report_4_winter = list(x = seq(0, 0.06, length.out = n_points),
+                                 density = dunif(seq(0, 0.1, length.out = n_points), 0, 0.1)),
     # Normal prior for sigma
     sigma = list(x = seq(0.5, 1, length.out = n_points),
                  density = dnorm(seq(0.5, 1, length.out = n_points), 0.75, 0.075)),
@@ -26,35 +32,43 @@ create_prior_df <- function(n_points = 1000) {
                       density = dunif(seq(0, 10/100, length.out = n_points), 0, 10/100)),
     season_offset = list(x = seq(0, 50/100, length.out = n_points),
                          density = dunif(seq(0, 50/100, length.out = n_points), 0, 50/100)),
-    D_immun = list(x = seq(0.5, 14, length.out = n_points),
-                   density = dunif(seq(0.5, 14, length.out = n_points), 0.5, 14)),
+    D_immun = list(x = seq(0.5, 18, length.out = n_points),
+                   density = dunif(seq(0.5, 18, length.out = n_points), 0.5, 18)),
     # Log-uniform prior for AKI hospitalisation
-    aki_hospitalisation_4 = list(
-      x = seq(0.0001, 0.75, length.out = n_points),
-      density = dunif(seq(0.0001, 0.75, length.out = n_points), 0.0001, 0.75)
+    # aki_hospitalisation_4_summer = list(
+    #   x = seq(0.0000001, 0.75, length.out = n_points),
+    #   density = dunif(seq(0.0000001, 0.75, length.out = n_points), 0.0000001, 0.75)
+    # ),
+    aki_hospitalisation_4_winter = list(
+      x = seq(0.0000001, 0.75, length.out = n_points),
+      density = dunif(seq(0.0000001, 0.75, length.out = n_points), 0.0000001, 0.75)
     ),
     # Log-normal priors for transmission probabilities
     probT_under5 = list(
-      x = seq(0.0001, 0.44, length.out = n_points),
-      density = dnorm(seq(0.0001, 0.4, length.out = n_points), mean = 0.21, sd = 0.115)
+      x = seq(0.0000001, 0.5, length.out = n_points),
+      density = dnorm(seq(0.0000001, 0.5, length.out = n_points), mean = 0.21, sd = 0.115)
     ),
     probT_over5 = list(
-      x = seq(0.0001, 0.114, length.out = n_points),
-      density = dnorm(seq(0.0001, 0.114, length.out = n_points), 0.05, 0.032)
+      x = seq(0.0000001, 0.114, length.out = n_points),
+      density = dnorm(seq(0.0000001, 0.114, length.out = n_points), mean = 0.05, sd = 0.032)
     ),
     # Log-uniform priors for gastro parameters
-    gastro_hospitalisation_4 = list(
-      x = seq(0.0001, 0.5, length.out = n_points),
-      density = dunif(seq(0.0001, 0.5, length.out = n_points), 0.0001, 0.5)
+    # gastro_hospitalisation_4_summer = list(
+    #   x = seq(0.0000001, 0.5, length.out = n_points),
+    #   density = dunif(seq(0.0000001, 0.5, length.out = n_points), 0.0000001, 0.5)
+    # ),
+    gastro_hospitalisation_4_winter = list(
+      x = seq(0.0000001, 0.5, length.out = n_points),
+      density = dunif(seq(0.0000001, 0.5, length.out = n_points), 0.0000001, 0.5)
     ),
     gastro_gp_attend_1 = list(
-      x = seq(0.0001, 0.5, length.out = n_points),
-      density = dunif(seq(0.0001, 0.5, length.out = n_points), 0.0001, 0.5)
-    ),
-    gastro_gp_attend_2 = list(
-      x = seq(0.0001, 0.5, length.out = n_points),
-      density = dunif(seq(0.0001, 0.5, length.out = n_points), 0.0001, 0.5)
+      x = seq(0.0000001, 0.75, length.out = n_points),
+      density = dunif(seq(0.0000001, 0.75, length.out = n_points), 0.0000001, 0.75)
     )
+    # gastro_gp_attend_2 = list(
+    #   x = seq(0.0000001, 0.75, length.out = n_points),
+    #   density = dunif(seq(0.0000001, 0.75, length.out = n_points), 0.0000001, 0.75)
+    # )
   )
   
   # Convert to data frame
